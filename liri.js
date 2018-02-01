34d26ff8ff8e8027de9f28605e0769387a007f7f
Marcelo Del Solar <solarg7@Marcelos-MacBook-Pro.local>
2018-01-31 23:15:06 -0500

    HW node js

--- liri.js
+++ liri.js
@@ -26,7 +26,7 @@ function functionLiri(action){
         break;
 
         case "do-what-it-says":
-        getAction();
+        getRandomAction();
         break;
         
     }
@@ -47,6 +47,7 @@ function getTweets(){
                     tweets[i].text + "\ndate: " + 
                     tweets[i].created_at;
                 console.log(packOutPut);
+                console.log("==============");
             }
         }else {
             //console.log("fallo");
@@ -57,11 +58,34 @@ function getTweets(){
 
 }
 
+function lookupValue() {
+    //concatenate the other process args together into one string
+        var lookup = "";
+        for (i = 3; i < wordInput.length; i++) {
+            lookup += wordInput[i] + " "; 
+        }
+        //console.log(lookup);
+        return lookup.trim(); // remove trailing space   
+}
+
+
+
 function getSong(){
+    if (wordInput.length != 3) {
+        var songSearch = lookupValue();
+        //console.log(wordInput);
+        
+    } else { 
+       //console.log(wordInput);  
+        var songSearch = "The Sign";
+    };
+
+
     //console.log("pao");
-    var songSearch = 
+    
     var spotify = new Spotify(keys.spotify);
-    spotify.search({ type: "track", query: "Faithfully"}, function(err, data){
+    //console.log(songSearch);
+    spotify.search({ type: "track", query: songSearch}, function(err, data){
         if (err) {
             console.log("error :" + err);
         }
@@ -82,15 +106,7 @@ function getSong(){
     });
 }
 
-function lookupValue() {
-    //concatenate the other process args together into one string
-        var lookup = "";
-        for (i = 3; i < wordInput.length; i++) {
-            lookup += wordInput[i] + " "; 
-        }
-        console.log(lookup);
-        return lookup.trim(); // remove trailing space   
-}
+
 
 function getMovie() {
     // if no movie title was input, use Mr. Nobody
@@ -111,6 +127,7 @@ function getMovie() {
         if (!error && response.statusCode === 200) {
             // the parse string into object
             var movieBody = JSON.parse(body);
+            console.log("=============");
             console.log("Title: " + movieBody.Title
                 + "\nYear: " + movieBody.Year
                 + "\nIMDB Rating: " + movieBody.imdbRating
@@ -123,17 +140,24 @@ function getMovie() {
     });
 }
 
-function getAction() {
-    fs.readFile("random.txt", "utf8", function(error, data) {
-        console.log(data);
-        inputString = data.split(","); // replace process.arg inputString with values in file
-        inputString.unshift("0","1");  // prepend two dummy values to match expected inputString
-        if (inputString[2] != "do-what-it-says") {
-            navigateToLIRI(inputString[2]);
-        } else {
-            console.log("Error. File value cannot call 'do-what-it-says'.")
-        }
+function getRandomAction() {
+    fs.readFile("./random.txt", "utf8", function(error, data) {
+        //console.log(data);
+        //console.log(data[1]);
+        var randomInput = data.split(","); // replace process.arg wordInput with values in file
+        wordInput[2] = randomInput[0];
+        wordInput[3] = randomInput[1];
+        var aux =  wordInput[2] ;
+        functionLiri(aux);
+        
+
+        //wordInput.unshift("0","1");  // prepend two dummy values to match expected wordInput
+        //if (wordInput[2] != "do-what-it-says") {
+            //navigateToLIRI(wordInput[2]);
+        //} else {
+            //console.log("Error. File value cannot call 'do-what-it-says'.")
     });
+    
 }
     
 
